c848fb806dfebbeed4e7dd4caab20cd1140f35a5
EngageQR Developer <user@engageqr.com>
2026-01-29 10:03:37 +0100

    Revert "Napraw animowane tło - użyj fixed position i z-index"
    This reverts commit 263fb080c45259ab25f12e8491b1421600558e14.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,21 +3,19 @@ import { Header, Footer } from '@/components/landing'
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col bg-white relative">
+    <div className="min-h-screen flex flex-col landing-canvas relative">
       <AnimatedBackgroundCSS />
-      <div className="relative z-10 flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-1">
-          <Hero />
-          <HowItWorks />
-          <Features />
-          <Pricing />
-          <Analytics />
-          <FAQ />
-          <CTA />
-        </main>
-        <Footer />
-      </div>
+      <Header />
+      <main className="flex-1">
+        <Hero />
+        <HowItWorks />
+        <Features />
+        <Pricing />
+        <Analytics />
+        <FAQ />
+        <CTA />
+      </main>
+      <Footer />
     </div>
   )
 }
